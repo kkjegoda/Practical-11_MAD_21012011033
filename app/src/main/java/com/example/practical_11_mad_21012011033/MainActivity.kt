b42f17513b731fbe4ea4c6388b063f8aa56d0e2a
kj <kkjegoda@gmail.com>
2023-11-02 19:52:58 +0530

    prac-11.1

--- app/src/main/java/com/example/practical_11_mad_21012011033/MainActivity.kt
+++ app/src/main/java/com/example/practical_11_mad_21012011033/MainActivity.kt
@@ -1,7 +1,93 @@
 package com.example.practical_11_mad_21012011033
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.view.Menu
+//import android.view.MenuItem
+//import android.widget.ListView
+//import androidx.recyclerview.widget.RecyclerView
+//import com.google.android.material.floatingactionbutton.FloatingActionButton
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.launch
+//import kotlinx.coroutines.withContext
+//import org.json.JSONArray
+//import org.json.JSONException
+//import org.json.JSONObject
+//
+//class MainActivity : AppCompatActivity() {
+//    private lateinit var personListView:ListView
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        setSupportActionBar(findViewById(R.id.toolbar))
+//
+//        personListView = findViewById(R.id.listview1)
+//
+//        val btn:FloatingActionButton = findViewById(R.id.floating_button_1)
+//        btn.setOnClickListener{
+//            setPersondata_to_Listview()
+//        }
+//    }
+//    fun setPersondata_to_Listview(){
+//        CoroutineScope(Dispatchers.IO).launch {
+//            try {
+//                val data = HttpRequest().makeServiceCall(
+//                    " https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+//                    "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1")
+//                withContext(Dispatchers.Main) {
+//                    try {
+//                        if(data != null)
+//                            runOnUiThread{getPersonDetailsFromJson(data)}
+//                    } catch (e: Exception) {
+//                        e.printStackTrace()
+//                    }
+//                }
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.main_menu,menu)
+//        return true
+//
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when(item.itemId) {
+//            R.id.action_button1 ->
+//            {
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+//
+//
+//    private fun getPersonDetailsFromJson(sJson: String?) {
+//        val personList = ArrayList<Person>()
+//        try {
+//            val jsonArray = JSONArray(sJson)
+//            for (i in 0 until jsonArray.length()) {
+//                val jsonObject = jsonArray[i] as JSONObject
+//                val person = Person(jsonObject)
+//                personList.add(person)
+//            }
+//            var listView1 = findViewById<RecyclerView>(R.id.listview1)
+//            listView1.adapter = PersonAdapter(this, personList)
+//        } catch (ee: JSONException) {
+//            ee.printStackTrace()
+//        }
+//    }
+//}
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
@@ -13,23 +99,27 @@ import org.json.JSONException
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var personListView:ListView
+    private lateinit var personRecyclerView : RecyclerView
+    lateinit var db:DatabaseHelper
+    val personList = ArrayList<Person>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        personListView = findViewById(R.id.listview1)
-
-        val btn:FloatingActionButton = findViewById(R.id.floating_button_1)
-        btn.setOnClickListener{
-            setPersondata_to_Listview()
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val btn= findViewById<FloatingActionButton>(R.id.floating_button_1)
+        personRecyclerView = findViewById(R.id.listview1)
+        personRecyclerView.adapter = PersonAdapter(this,personList)
+        db = DatabaseHelper(this)
+        getPersonDetailsFromSqliteDb()
+        btn.setOnClickListener(){
+            JsonDb()
         }
     }
-    fun setPersondata_to_Listview(){
+    fun JsonDb(){
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 val data = HttpRequest().makeServiceCall(
-                    " https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+                    "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
                     "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1")
                 withContext(Dispatchers.Main) {
                     try {
@@ -45,19 +135,68 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_button1->{
+                getPersonDetailsFromSqliteDb()
+                true}
+            R.id.action_button2->{
+                JsonDb()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun getPersonDetailsFromJson(sJson: String?) {
-        val personList = ArrayList<Person>()
+
+        val size = personList.size
+        personList.clear()
+        personRecyclerView.adapter?.notifyItemRangeRemoved(0,size)
         try {
             val jsonArray = JSONArray(sJson)
             for (i in 0 until jsonArray.length()) {
                 val jsonObject = jsonArray[i] as JSONObject
                 val person = Person(jsonObject)
                 personList.add(person)
+                try {
+                    if (db.getPerson(person.id) != null)
+                        db.updatePerson(person)
+                    else
+                        db.insertPerson(person)
+                }catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
-            var listView1 = findViewById<RecyclerView>(R.id.listview1)
-            listView1.adapter = PersonAdapter(this, personList)
+            personRecyclerView.adapter?.notifyItemRangeInserted(0,personList.size)
+
         } catch (ee: JSONException) {
             ee.printStackTrace()
         }
+        Toast.makeText(this, "Fetched details from JSON", Toast.LENGTH_SHORT).show()
     }
+    private fun getPersonDetailsFromSqliteDb(){
+        val size = personList.size
+        personList.clear()
+        personRecyclerView.adapter?.notifyItemRangeRemoved(0,size)
+        try {
+            personList.addAll(db.allPersons)
+            personRecyclerView.adapter?.notifyItemRangeInserted(0,personList.size)
+        }catch (ee: JSONException) {
+            ee.printStackTrace()
+        }
+        Toast.makeText(this, "Fetched details from Sqlite database", Toast.LENGTH_SHORT).show()
+    }
+    public fun delete(position:Int){
+        db.deletePerson(personList[position])
+        personList.removeAt(position)
+        personRecyclerView.adapter?.notifyItemRemoved(position)
+    }
+
 }
+
--- app/src/main/java/com/example/practical_11_mad_21012011033/Persontable.kt
+++ app/src/main/java/com/example/practical_11_mad_21012011033/Persontable.kt
@@ -1,25 +1,24 @@
 package com.example.practical_11_mad_21012011033
 
-class Persontable {
+class PersonTable {
     companion object{
-        val TABLE_NAME="Person"
-        val COLUMN_ID="Id"
-        val COLUMN_NAME="Name"
-        val COLUMN_PHONENO="Number"
-        val COLUMN_EMAIL_ID="Email_id"
-        val COLUMN_ADDRESS="Address"
-        val COLUMN_GPS_LAT="Gps_lat"
-        val COLUMN_GPS_LONG="Gps_long"
+        val TABLE_NAME = "person"
+        val COLUMN_ID ="id"
+        val COLUMN_NAME ="person_name"
+        val COLUMN_PERSON_EMAIL_ID = "person_email_id"
+        val COLUMN_PERSON_PHONE_NO = "person_phone_no"
+        val COLUMN_PERSON_ADDRESS = "person_address"
+        val COLUMN_PERSON_GPS_LAT = "person_lat"
+        val COLUMN_PERSON_GPS_LOG = "person_log"
 
-        val CREATE_TABLE=("CREATE TABLE "+ TABLE_NAME+"("+ COLUMN_ID+"TEXT PRIMARY KEY,"
-                + COLUMN_NAME+"TEXT,"
-                + COLUMN_PHONENO+"TEXT,"
-                + COLUMN_EMAIL_ID+"TEXT,"
-                + COLUMN_ADDRESS+ "TEXT,"
-                + COLUMN_GPS_LAT+"REAL,"
-                + COLUMN_GPS_LONG+"REAL"+")")
-
-
-
+        val CREATE_TABLE =("CREATE_TABLE" + TABLE_NAME + "("
+                + COLUMN_ID + "TEXT PRIMARY KRY,"
+                + COLUMN_NAME + "TEXT,"
+                + COLUMN_PERSON_EMAIL_ID + "TEXT,"
+                + COLUMN_PERSON_PHONE_NO + "TEXT,"
+                + COLUMN_PERSON_ADDRESS + "TEXT,"
+                + COLUMN_PERSON_GPS_LAT + "REAL,"
+                + COLUMN_PERSON_GPS_LOG + "REAL,"
+                + ")")
     }
 }
